ad333556b89f8e1502343862bce48abfb9dcd604
baptiste.chocot@telecomnancy.eu <baptiste.chocot@telecomnancy.eu>
2019-01-14 14:48:16 +0100

    remane files, add lil things

--- Main.py
+++ Main.py
@@ -8,23 +8,27 @@ import time
 import simpy.rt
 from Simulateur import Poisson
 
-test_seconds_duration = 10
+def main():
+    test_seconds_duration = 10
 
-'''Il s'agit du fichier principal de notre projet qui lance les différentes parties et fait le lien entre elles'''
-start_time = time.time()
-poisson = Poisson.Poisson()
-print(poisson.generate())
-
-
-'''Load <...>'''
-env = simpy.rt.RealtimeEnvironment(factor=0.1)
-env.run(until=test_seconds_duration*10)
+    '''Il s'agit du fichier principal de notre projet qui lance les différentes parties et fait le lien entre elles'''
+    start_time = time.time()
+    poisson = Poisson.Poisson()
+    print(poisson.generate())
 
 
+    '''Load <...>'''
+    env = simpy.rt.RealtimeEnvironment(factor=0.1)
+    env.run(until=test_seconds_duration*10)
 
-'''Building <...>'''
-#<...>
-print("Execution time : " + time.strftime("%M:%S", time.localtime(time.time() - start_time)))
 
-'''Establish <...>'''
 
+    '''Building <...>'''
+    #<...>
+    print("Execution time : " + time.strftime("%M:%S", time.localtime(time.time() - start_time)))
+
+    '''Establish <...>'''
+
+# starting program
+if __name__ == "__main__":
+    main()

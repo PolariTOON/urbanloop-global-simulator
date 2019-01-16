41f02da9009969e87170e4c84a58f873a10f7585
HENRY Thibault (Kazź) <thibault.henry@telecomnancy.net>
2019-01-16 11:59:55 +0100

    Station type

--- model/station.py
+++ model/station.py
@@ -1,7 +1,15 @@
 #! /usr/bin/env python3
 # coding: utf-8
+from enum import Enum
 
 station_id = 0
+
+
+class Type(Enum):
+    NEUTRAL = 0
+    ACTIVITY = 1
+    RESIDENTIAL = 2
+    CITY = 3
 
 
 class Station:
@@ -10,7 +18,7 @@ class Station:
     # network
     network = None
 
-    def __init__(self, name=None, capacity=1, previous_station=None, next_station=None, next_switch=None, loop=None):
+    def __init__(self, name=None, capacity=1, previous_station=None, next_station=None, next_switch=None, loop=None, type = Type.NEUTRAL):
         global station_id
         self.id = station_id
         station_id += 1
@@ -21,6 +29,7 @@ class Station:
         self.next_station = next_station
         self.nex_switch = next_switch
         self.loop = loop
+        self.type = type
         self.queue = 0
 
     def _update_flow(self):

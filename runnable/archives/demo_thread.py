884f7123e40464ff63c39b1b6cd66725776a6c74
chocorean <baptiste.chocot@gmail.com>
2019-03-06 12:41:31 +0100

    test

--- runnable/archives/demo_thread.py
+++ runnable/archives/demo_thread.py
@@ -1,23 +1,27 @@
-from threading import Thread
+import threading as thd
 import time
 import datetime as dt
 
 
 def fun_th1():
+    th = thd.current_thread()
+    print(th.name)
     while True:
-        print("[{0}] yo".format(dt.datetime.now().strftime("%H:%M:%S")))
+        print("[{0}][{1}] yo".format(dt.datetime.now().strftime("%H:%M:%S"), thd.get_ident()))
         time.sleep(1)
 
 
 def fun_th2():
+    th = thd.current_thread()
+    print(th.name)
     while True:
-        print("[{0}] mdr".format(dt.datetime.now().strftime("%H:%M:%S")))
+        print("[{0}][{1}] mdr".format(dt.datetime.now().strftime("%H:%M:%S"), thd.get_ident()))
         time.sleep(5)
 
 
 def main():
-    th1 = Thread(target=fun_th1)
-    th2 = Thread(target=fun_th2)
+    th1 = thd.Thread(target=fun_th1)
+    th2 = thd.Thread(target=fun_th2)
     th1.start()
     th2.start()
     return

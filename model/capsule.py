29090ec61ca891a794f879dfa3aa8e6fa50ad47d
ABCharlotte <charlotte.boulle@telecomnancy.net>
2019-03-11 11:11:27 +0100

    fixer distances

--- model/capsule.py
+++ model/capsule.py
@@ -90,6 +90,7 @@ class Capsule:
         :return: Trip event generator
         """
         dist_to_next_element = self.loop.distance_between(self.current_element, self.next_element)
+        # print(self.current_element.name, self.next_element.name, dist_to_next_element)
         self.segment_length = dist_to_next_element
         time_to_next_element = dist_to_next_element / self.speed
         self.segment_start_tick = sim_loop.get_current_tick()
--- model/loop.py
+++ model/loop.py
@@ -59,21 +59,27 @@ class Loop:
                 else:
                     self.objects += [["warehouse", element, element.angle]]
         # mise à jour des longueurs
+        nb_elements = len(self.objects)
+        for i in range(nb_elements):
+            an_object = self.objects[i]
+            next_object = self.objects[(i-1)%nb_elements]
             if self.clockwise:  # sens horaire des angles
-                angle_to_go = self.objects[i][2] - self.objects[(i-1)%len(self.objects)][2]
+                angle_next = an_object[2] - next_object[2]
             else:  # sens trigonométrique des angles
-                angle_to_go = self.objects[(i-1) % len(self.objects)][2] - self.objects[i][2]
-            if angle_to_go < 0:
-                angle_to_go += 360
-            self.lengths[i-1] = float(angle_to_go / 360) * self.size  # arc = D*pi*angle/360  et D = circonference/pi
+                angle_next = next_object[2] - an_object[2]
+            if angle_next < 0:
+                angle_next += 360
+            print(an_object[1].name, next_object[1].name, angle_next)
+            self.lengths[i] = float(angle_next / 360) * self.size  # arc = D*pi*angle/360  et D = circonference/pi
             if self.size is None:
                 self.size = sum(self.lengths)
+        print(self.lengths)
 
     def distance_between(self, element1, element2):
         for i in range(len(self.objects)):
             element = self.objects[i][1]
             if element is element1:
-                if element is element2: # on veut la taille du switch element1
+                if element is element2:  # on veut la taille du switch element1
                     return element.size
                 distance = 0
                 for j in range(i + 1, len(self.objects) + i + 1):

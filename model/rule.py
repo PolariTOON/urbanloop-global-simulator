5dc18a5a63618e70533304fc7c23dc829a6638c5
Victor <victor.thevenon@telecomnancy.eu>
2019-04-11 12:30:21 +0200

    Ajout graphe, rule

---
+++ model/rule.py
@@ -0,0 +1,9 @@
+class Rule:
+    def __init__(self,destination=None,priority=0,switch_state=None,id_pod=None,time=None,num_rule=None,change=None):
+        self.destination = destination
+        self.priority = priority
+        self.switch_state = switch_state
+        self.id_pod = id_pod
+        self.time = time
+        #self.num_rule = num_rule
+        self.change = change

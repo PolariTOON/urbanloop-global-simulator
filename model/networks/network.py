0fb4c4d5c1c19dc46d3c7a75a5612035e9b20811
Malo Mongard <malo.mongard@telecomnancy.eu>
2019-06-17 09:11:22 +0200

    Structure pour le chargement du json + json d'exemple ok

--- model/networks/network.py
+++ model/networks/network.py
@@ -2,19 +2,20 @@
 Cette classe gère un réseau entier, c'est le niveau meta-graph du réseau
 les noeuds peuvent être des routes (partie interne d'une boucle) ou des ponts (pour relier les boucles)
 """
+import json
 
-from ..node import Node
+from ..node2 import Node
 
 
 class Network(Node):
-    def __init__(self):
+    def __init__(self, json_network_path):
         self._bridges = []
         self._loops = []
         self._roads = []
 
     @property
     def capsules(self):
-        return [capsule for road in self.roads for capsule in road.capsules]
+        return [capsule for road in self._roads for capsule in road.capsules]
 
     @property
     def bridges(self):
@@ -30,5 +31,31 @@ class Network(Node):
             'loops': [loop.serialize() for loop in self.loops],
         })
 
-    def init_graph_from_json(self, json_network):
-        for loop_name, info in json_network.items():
+    def init_graph_from_json(self, json_network_path):
+        with open(json_network_path) as json_data:
+            json_network = json.load(json_data)
+        loops = json_network["loops"]
+        bridges = json_network["bridges"]
+        capsules = json_network["capsules"]
+        for bridge in bridges:
+            path = bridge["type"]
+            # TODO : charger les bridges
+            new_bridge = Bridge()
+            self._bridges.append(new_bridge)
+
+        for loop in loops:
+            loop_name = loop["name"]
+            clockwise = loop["clockwise"]
+            elements = loop["elements"]
+            paths = loop["paths"]
+            # TODO : charger les boucles
+            new_loop = Loop()
+            self._loops.append(new_loop)
+
+        for capsule in capsules:
+            # TODO : charger les capsules
+            new_capsule = Capsule()
+            for loop in self._loops:
+                if new_capsule.loop_id == loop.id:
+                    loop._capsules.append(new_capsule)
+            pass

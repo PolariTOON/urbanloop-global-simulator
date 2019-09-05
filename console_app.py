c24818191f96a261e8cc3a9a390168de34f6b888
Malo Mongard <malo.mongard@telecomnancy.eu>
2019-09-05 19:20:03 +0200

    Ajout de console app

--- console_app.py
+++ console_app.py
@@ -1,22 +1,40 @@
 """
 Cette classe permet de gérer le code lié à un lancement d'une simulation sans interface graphique
 """
+from asyncio import get_running_loop, run
+from json import load
+from threading import Thread
 
-from time import time
-from model import routing, switch
-from settings import config, network, simlog
-from simulator import converter, sim_loop
+from controler.simulation import Simulation
+
+_network_file = "resources/new_mini_network.json"  # TODO: mettre cette ou ces valeurs en paramètre du programme
+_sim_tick = 0.042  # TODO: mettre cette ou ces valeurs en paramètre du programme
+_loop = None
+_simulations = {}
+
+
+async def _run_simulations():
+    """Lancement de simulations"""
+    print("Log format : receiver  --  author  --  message type")
+    global _sim_tick
+    global _loop
+    global _simulations
+    _loop = get_running_loop()
+    while True:
+        for key in _simulations:
+            simulation = _simulations[key]
+            simulation.update()
 
 
 def run_app():
-    START_TIME = time()
-    config.restore_config()
-    routing.init_routing()
-    switch.init_switch()
-    converter.init_converter()
-    simlog.load()
-    network.load()
-    simlog.debug("Simulation starts")
-    sim_loop.start_simulation()
-    simlog.debug("Execution time : %.3f seconds" % (time() - START_TIME))
-    simlog.debug("Simulation time : %.1f seconds" % (sim_loop.get_simulated_time()))
+    with open(_network_file) as file:
+        network_index = 0
+        network_item = load(file)
+    if network_item is not None:
+        if "id" in network_item:
+            del network_item["id"]
+        simulation = Simulation(network_index, _sim_tick, **network_item)
+        _simulations[network_index] = simulation
+    elif network_index in _simulations:
+        del _simulations[network_index]
+    Thread(target=lambda: run(_run_simulations())).start()
--- main.py
+++ main.py
@@ -12,7 +12,6 @@ argument_parser = ArgumentParser(description="Run the UrbanLoop simulator")
 argument_parser.add_argument("-p", "--port", type=port, default=-1, const=8090, nargs='?', help="run a web application on the given port or 8090 by default")
 arguments = argument_parser.parse_args()
 
-
 if arguments.port is -1:
     from console_app import run_app
     run_app()

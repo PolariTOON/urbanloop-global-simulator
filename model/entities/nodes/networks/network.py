0378746cd94a7133f4217fcef33c7c2ad0ed3dfb
valentin gau <valentin.gau@telecomnancy.eu>
2019-11-28 16:09:38 +0100

    modifications sur la gestion du nouveu modèle de stations

--- model/entities/nodes/networks/network.py
+++ model/entities/nodes/networks/network.py
@@ -288,28 +288,62 @@ class Network(Node):
                 del loop["id"]
             self._loops[b] = Loop(b, **loop)
 
-    def _init_station(self):
-    	#mise à jour de l'initialisation d'une station, afin de modéliser les voies d'entrée et de sortie à une station.
-    	#on y rajoute un switch in et un switch out sur la boucle qui contient la gare afin d'y entrer et d'y sortir
-    	#on crée une nouvelle boucle pour la station qui va contenir un switch in pour rentrer dans la station, un switch out pour en sortir et la station entre les 2 ainsi que 3 sections pour relier ces éléments
-    	#cela implique aussi de rajouter les 2 bridges nécessaires pour relier les switchs
+    def _init_station(self, max_speed, places_number):
+    	#Mise à jour de l'initialisation d'une station, afin de modéliser les voies d'entrée et de sortie à une station.
+    	#On y rajoute un switch in et un switch out sur la boucle qui contient la gare afin d'y entrer et d'y sortir
+    	#On crée une nouvelle boucle pour la station qui va contenir un switch in pour rentrer dans la station, 
+    	#un switch out pour en sortir et la station entre les 2 ainsi que 3 sections pour relier ces éléments
+    	#on crée aussi un switch in et un switch out pour entrer et sortir de la boucle principale pour aller dans la station
+    	#Cela implique aussi de rajouter les 2 bridges nécessaires pour relier les switchs
     	id_loop = len(self._loops)
     	new_loop = Loop(id_loop)
-    	
+
+    	self._loops[id_loop] = new_loop
+    	steps0 = []
+    	steps1 = []
+        elements = self._loops[id_loop]["elements"]
+        roads = self._loops[id_loop]["roads"]
+
     	id_road = len(self._roads)
-    	new_road1 = Road(env, id_road, self._margin_min, self._pod_size, False)
-    	new_road2 = Road(env, id_road + 1, self._margin_min, self._pod_size, False)
+    	new_road0 = Road(env, id_road, self._margin_min, self._pod_size, False)
+    	new_road1 = Road(env, id_road + 1, self._margin_min, self._pod_size, False)
 
     	id_station = len(self._stations)
     	new_station = Station(env, id_station, name="test")
 
     	id_switch = len(self._switches)
-    	new_switch_in = SwitchIn(env, id_switch, self._margin_min, self._pod_size, max_speed, places_number)
-    	new_switch_out = SwitchOut(env, id_switch + 1, self._margin_min, self._pod_size, max_speed)
+    	new_switch_in_loop = SwitchIn(env, id_switch, self._margin_min, self._pod_size, max_speed, places_number)
+    	new_switch_out_loop = SwitchOut(env, id_switch + 1, self._margin_min, self._pod_size, max_speed)
+    	new_switch_in_station = SwitchIn(env, id_switch + 2, self._margin_min, self._pod_size, max_speed, places_number)
+    	new_switch_out_station = SwitchOut(env, id_switch + 3, self._margin_min, self._pod_size, max_speed)
 
-    	new_section0 = Section(env, 0, margin_min, pod_size, is_bridge)
-    	new_section1 = Section(env, 1, margin_min, pod_size, is_bridge)
-    	new_section2 = Section(env, 2, margin_min, pod_size, is_bridge)
+    	new_section0 = Section(env, 0, self.margin_min, self.pod_size, is_bridge)
+    	new_section1 = Section(env, 1, self.margin_min, self.pod_size, is_bridge)
+    	new_section2 = Section(env, 2, self.margin_min, self.pod_size, is_bridge)
+
+
+    	new_road0.append({
+                "steps": new_station,
+                "sections": [new_section0, new_section1]
+            })
+
+    	new_road1.append({
+                "sections": [new_section2]
+            })
+
+    	id_bridge = len(self.bridges)
+    	new_bridge_0 = Bridge(self, id_bridge)
+    	new_bridge_1 = Bridge(self, id_bridge)
+
+    	new_bridge_0["switches"] = [new_switch_out_loop, new_switch_in_station]
+    	self._init_pods_of_line(new_bridge_0)
+    	self._bridges[id_bridge] = Bridge(id_bridge, **new_bridge_0)
+
+    	new_bridge_1["switches"] = [new_switch_out_station, new_switch_in_loop]
+    	self._init_pods_of_line(new_bridge_1)
+    	self._bridges[id_bridge+1] = Bridge(id_bridge+1, **new_bridge_1)
+
+
 
     def _init_parent_of_children(self):
         """

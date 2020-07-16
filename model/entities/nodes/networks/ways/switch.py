8d036e980bf3e9d6814f346447c8925f80a9459a
Pierre-Antoine Grosselin <pierre-antoine.grosselin@telecomnancy.eu>
2020-07-16 10:58:35 +0200

    suppr print

--- model/entities/nodes/networks/ways/switch.py
+++ model/entities/nodes/networks/ways/switch.py
@@ -17,7 +17,6 @@ class Switch(Way):
         self._id_bridge = id_bridge
         self._rules = []
         self._margin = self.speed * (margin_min + pod_size) / 8.33 - pod_size # Ce 8.33 est actuellement la vitesse minimal d'un réseau urbanloop en m/s (30 km/h) peut etre à changer
-        print("test speed", self.name, self.speed)
 
         # Ajout des capsules
         for pod in pods:

96a756f7b7868bc104f961b542558f68fc69869c
Tommy Barafani <tommy.barafani@telecomnancy.eu>
2022-04-18 12:05:47 +0200

    Affichage Hint Hanger corrigé

--- view/static/HangarSimple.js
+++ view/static/HangarSimple.js
@@ -72,7 +72,7 @@ export class HangarSimple extends Entity {
         this.__outerShape.fill(outerColor);
     }
     update(json) {
-        const name = json["name"] + "Hangar";
+        const name = json["name"] + "\nHangar";
         const x = json["x"];
         const y = json["y"];
         const podCount = json["pods"]["count"];

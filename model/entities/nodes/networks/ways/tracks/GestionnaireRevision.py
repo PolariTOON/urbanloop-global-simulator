16409ec5e0aff4556a8a13b5fa6033b89b4bd2f7
Tommy Barafani <tommy.barafani@telecomnancy.eu>
2022-05-04 15:57:14 +0200

    Correction d'une mauvaise condition de if introduite en 89e05b43eb2a78c875bb176fada885b349ba01b6

--- model/entities/nodes/networks/ways/tracks/GestionnaireRevision.py
+++ model/entities/nodes/networks/ways/tracks/GestionnaireRevision.py
@@ -79,5 +79,5 @@ class GestionnaireRevision:
         return \
                 pod.distance_depuis_revision >= self._limite_distance_parcourue_avant_revision \
                 or pod.temps_depuis_revision >= self._limite_temps_ecoule_avant_revision \
-                or (pod.nombre_signalements_doit_aller_en_revision >= self._limite_nombre_signalements_avant_revision
-                    and pod.dernier_signalement_revision_par_vrai_utilisateur)
+                or pod.nombre_signalements_doit_aller_en_revision >= self._limite_nombre_signalements_avant_revision \
+                or pod.dernier_signalement_revision_par_vrai_utilisateur

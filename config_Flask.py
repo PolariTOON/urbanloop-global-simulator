7922fda41ea6098d5fa3a07ba800b45e42d7c523
Tommy Barafani <tommy.barafani@telecomnancy.eu>
2022-05-04 15:05:29 +0200

    Correction des copier-coller défectueux de 14a5132b42a5ed92f8cfc79e47dc5f7ab04849fd

--- config_Flask.py
+++ config_Flask.py
@@ -365,28 +365,33 @@ def call_emergency_exit(user_id):
 
 
 #p_tbtc
-@_app.route('/envoi_lavage/<string:user_id>', methods=['POST'])
-def demander_envoi_au_lavage(user_id):
-    """ Fonction permettant de demander l'envoi d'une capsule au lavage après le voyage, par l'utilisateur qui est dedans """
+@_app.route('/capsule_revision/<string:pod_id>', methods=['POST'])
+def demander_revision_capsule(pod_id):
+    """Fonction permettant de demander l'envoi d'une capsule en révision après le voyage, par son pod_id"""
 
     global _simulation_for_api
-    if _simulation_for_api == None:
-        return jsonify({ 'msg': 'Aucune simulation n\'a été chargée' })
 
-    network = _simulation_for_api.get_network()
-    pod_of_user = network.get_pod_of_user(user_id)
+    if _simulation_for_api is None:
+        return jsonify({"msg": "Aucune simulation n'a été chargée"})
+    else:
 
-    if (pod_of_user == None):
-        return jsonify({ 'status_user': "En attente d'une capsule ou capsule non trouvee"})
-    else :
-        pod_of_user.demander_envoi_lavage(user_id)
-        pod_of_user.dernier_signalement_lavage_par_vrai_utilisateur = True
-        return jsonify({ 'msg': 'Demande d\'envoi au lavage faite' })
+        network = _simulation_for_api.get_network()
+        pod = network.get_pod_of_id(int(pod_id))
+        print("***")
+        print(pod.id if pod is not None else None)
+        print("***")
+
+        if pod is None:
+            return jsonify({"status_user": "Capsule non trouvée"})
+        else:
+            pod.demander_envoi_revision(None)
+            pod.dernier_signalement_revision_par_vrai_utilisateur = True
+            return jsonify({"msg": f"Demande d'envoi en révision pour la capsule {pod_id} faite"})
 
 
 #p_tbtc
 @_app.route('/capsule_lavage/<string:pod_id>', methods=['POST'])
-def demander_revision_capsule(pod_id):
+def demander_lavage_capsule(pod_id):
     """Fonction permettant de demander l'envoi d'une capsule au lavage après le voyage, par son pod_id"""
 
     global _simulation_for_api
@@ -410,31 +415,6 @@ def demander_revision_capsule(pod_id):
 
 
 #p_tbtc
-@_app.route('/capsule_revision/<string:pod_id>', methods=['POST'])
-def demander_lavage_capsule(pod_id):
-    """Fonction permettant de demander l'envoi d'une capsule à la révision après le voyage, par son pod_id"""
-
-    global _simulation_for_api
-
-    if _simulation_for_api is None:
-        return jsonify({"msg": "Aucune simulation n'a été chargée"})
-    else:
-
-        network = _simulation_for_api.get_network()
-        pod = network.get_pod_of_id(int(pod_id))
-        print("***")
-        print(pod.id if pod is not None else None)
-        print("***")
-
-        if pod is None:
-            return jsonify({"status_user": "Capsule non trouvée"})
-        else:
-            pod.demander_envoi_revision(None)
-            pod.dernier_signalement_revision_par_vrai_utilisateur = True
-            return jsonify({"msg": f"Demande d'envoi à la révision pour la capsule {pod_id} faite"})
-
-
-#p_tbtc
 @_app.route('/envoi_revision/<string:user_id>', methods=['POST'])
 def demander_envoi_en_revision(user_id):
     """ Fonction permettant de demander l'envoi d'une capsule en révision après le voyage, par l'utilisateur qui est dedans """
@@ -453,3 +433,22 @@ def demander_envoi_en_revision(user_id):
         pod_of_user.dernier_signalement_revision_par_vrai_utilisateur = True
         return jsonify({ 'msg': 'Demande d\'envoi en révision faite' })
 
+
+#p_tbtc
+@_app.route('/envoi_lavage/<string:user_id>', methods=['POST'])
+def demander_envoi_au_lavage(user_id):
+    """ Fonction permettant de demander l'envoi d'une capsule au lavage après le voyage, par l'utilisateur qui est dedans """
+
+    global _simulation_for_api
+    if _simulation_for_api == None:
+        return jsonify({ 'msg': 'Aucune simulation n\'a été chargée' })
+
+    network = _simulation_for_api.get_network()
+    pod_of_user = network.get_pod_of_user(user_id)
+
+    if (pod_of_user == None):
+        return jsonify({ 'status_user': "En attente d'une capsule ou capsule non trouvee"})
+    else :
+        pod_of_user.demander_envoi_lavage(user_id)
+        pod_of_user.dernier_signalement_lavage_par_vrai_utilisateur = True
+        return jsonify({ 'msg': 'Demande d\'envoi au lavage faite' })
